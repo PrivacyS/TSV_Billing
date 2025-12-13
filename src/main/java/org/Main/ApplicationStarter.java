59822d28d052a61d4f3a940fb1b7d88bc3e1cac9
Eric Horacsek <Eric Horacsek>
2025-12-13 13:19:09 +0100

    have fun

--- src/main/java/org/Main/ApplicationStarter.java
+++ src/main/java/org/Main/ApplicationStarter.java
@@ -1,11 +1,12 @@
 package org.Main;
 
+import GUI.Hoursheet_GUI;
 import javafx.application.Application;
 
 public class ApplicationStarter {
 
     public static void main(String[] args) {
-        Application.launch(Main.class, args);
+        Application.launch(Hoursheet_GUI.class, args);
     }
 
 }
