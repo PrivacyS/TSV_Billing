fe531b9b351bb1efadc4bb5fabe1e4fc2d4aecdb
st <lindner634@gmail.com>
2025-12-13 13:11:20 +0100

    Fixed

---
+++ src/main/java/org/Main/ApplicationStarter.java
@@ -0,0 +1,11 @@
+package org.Main;
+
+import javafx.application.Application;
+
+public class ApplicationStarter {
+
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
+
+}
