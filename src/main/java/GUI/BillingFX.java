3a06ff1679c0ab74cfdefdaf4d158e13582e48bf
Eric Horacsek <Eric Horacsek>
2025-12-18 09:12:01 +0100

    cant code without internet

--- src/main/java/GUI/BillingFX.java
+++ src/main/java/GUI/BillingFX.java
@@ -3,12 +3,13 @@ package GUI;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.control.DatePicker;
 
@@ -16,23 +17,39 @@ import java.time.LocalDate;
 import java.util.Date;
 
 public class BillingFX  {
-    private final BorderPane rootPane;
+    private final GridPane gridPane;
     private final Button test;
     private final DatePicker datePicker;
     public BillingFX(Button back) {
-        rootPane = new BorderPane();
+        Text sceneTitle = new Text("Übungsleiterstunden Abrechnung");
+        gridPane = new GridPane();
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        gridPane.setPadding(new Insets(25, 25, 25, 25));
+        TextField startTime = new TextField();
+        TextField endTime = new TextField();
+        TextField amount = new TextField(); // 45 or 60 minutes
+        TextField place = new TextField(); // which gym
+        TextField teamname = new TextField(); // Herren, Damen, U16 etc.
         test = back;
-        rootPane.setTop(test);
         datePicker = new DatePicker();
+        TextField[] fields = {startTime, endTime, amount, place};
+        gridPane.add(sceneTitle, 0, 0, 2, 1);
+        for(int i = 1; i < fields.length; i++) {
+            gridPane.add(fields[i], 0, i, 1, 1);
+        }
+        gridPane.add(test, 0, fields.length+2,1,1);
+
         datePicker.setOnAction(event -> {
             LocalDate date = datePicker.getValue();
             System.out.println(date);
         });
-        rootPane.setCenter(datePicker);
+        gridPane.add(datePicker, 0, fields.length + 1 ,1 ,1);
     }
 
-    public BorderPane getRootPane(){
-        return rootPane;
+    public GridPane getRootPane(){
+        return gridPane;
     }
 
 
--- src/main/java/GUI/Hoursheet_GUI.java
+++ src/main/java/GUI/Hoursheet_GUI.java
@@ -4,14 +4,18 @@ package GUI;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.control.DatePicker;
-
+import java.awt.Taskbar;
+import java.awt.Toolkit;
+import java.net.URL;
 import java.time.LocalDate;
 
 public class Hoursheet_GUI extends Application {
@@ -21,8 +25,26 @@ public class Hoursheet_GUI extends Application {
         VBox menuBox = new VBox();
         menuBox.setSpacing(10);
         Button uelBtn = new Button("Übungsleiterstunden abrechnen");
+        uelBtn.setMinSize(200, 5);
         Button back = new Button("back");
         Button trvlCostsBtn = new Button("Fahrtkosten abrechnen");
+        trvlCostsBtn.setMinSize(200,5);
+
+        if (Taskbar.isTaskbarSupported()) {
+            var taskbar = Taskbar.getTaskbar();
+
+            if (taskbar.isSupported(Taskbar.Feature.ICON_IMAGE)) {
+                // You generally need to load this via AWT Toolkit for the Mac Dock
+                final Toolkit defaultToolkit = Toolkit.getDefaultToolkit();
+                URL imageResource = getClass().getResource("/tsv-logo.jpg");
+
+                if (imageResource != null) {
+                    java.awt.Image image = defaultToolkit.getImage(imageResource);
+                    taskbar.setIconImage(image);
+                }
+            }
+        }
+
         back.setOnAction(a -> {
             primaryStage.getScene().setRoot(menuBox);
         });
@@ -38,10 +60,11 @@ public class Hoursheet_GUI extends Application {
         });
 
         menuBox.getChildren().addAll(uelBtn, trvlCostsBtn);
-
+        menuBox.setAlignment(Pos.CENTER);
         Scene scene = new Scene(menuBox, 1000, 1000);
 
         primaryStage.setTitle("TSV Neuburg 1862");
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/tsv-logo.jpg")));
         primaryStage.setScene(scene);
         primaryStage.show();
     }
