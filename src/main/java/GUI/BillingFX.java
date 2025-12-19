546359f28098bda6d6204f39488ed7e444621d08
Eric Horacsek <Eric Horacsek>
2025-12-19 13:14:41 +0100

    added some UI spagetthi-code

--- src/main/java/GUI/BillingFX.java
+++ src/main/java/GUI/BillingFX.java
@@ -1,17 +1,20 @@
 package GUI;
 
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.control.DatePicker;
+import javafx.collections.FXCollections;
 
 import java.time.LocalDate;
 import java.util.Date;
@@ -27,25 +30,30 @@ public class BillingFX  {
         gridPane.setHgap(10);
         gridPane.setVgap(10);
         gridPane.setPadding(new Insets(25, 25, 25, 25));
-        TextField startTime = new TextField();
-        TextField endTime = new TextField();
-        TextField amount = new TextField(); // 45 or 60 minutes
-        TextField place = new TextField(); // which gym
-        TextField teamname = new TextField(); // Herren, Damen, U16 etc.
+        TextField startTime = new TextField("Start des Trainings");
+        TextField endTime = new TextField("Ende des Trainings");
+        ComboBox trainingUnit = new ComboBox();
+        ObservableList<String> options = FXCollections.observableArrayList("45min.", "60min");
+        trainingUnit.setItems(options);
+        TextField place = new TextField("Trainingsort"); // which gym
+        TextField teamname = new TextField("Gruppenname"); // Herren, Damen, U16 etc.
         test = back;
         datePicker = new DatePicker();
-        TextField[] fields = {startTime, endTime, amount, place};
+        TextField[] fields = {startTime, endTime, place};
         gridPane.add(sceneTitle, 0, 0, 2, 1);
-        for(int i = 1; i < fields.length; i++) {
-            gridPane.add(fields[i], 0, i, 1, 1);
+        gridPane.add(trainingUnit, 0, 1, 3,2);
+        for(int i = 0; i < fields.length; i++) {
+            gridPane.add(fields[i], 0, i+4, 1, 1);
         }
-        gridPane.add(test, 0, fields.length+2,1,1);
+        gridPane.add(test, 0, fields.length+6,1,1);
 
         datePicker.setOnAction(event -> {
             LocalDate date = datePicker.getValue();
             System.out.println(date);
         });
-        gridPane.add(datePicker, 0, fields.length + 1 ,1 ,1);
+        gridPane.add(datePicker, 0, fields.length + 5,1 ,1);
+        gridPane.add(teamname,  0, fields.length + 4,1 ,1);
+
     }
 
     public GridPane getRootPane(){
